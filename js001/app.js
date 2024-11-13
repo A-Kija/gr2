3897bc15c6cdbca57938c9a1eefd3f2f09e66808
A-Kija <arvydas.kijakauskas@gmail.com>
2024-11-13 22:12:57 +0200

    001js

---
+++ js001/app.js
@@ -0,0 +1,33 @@
+console.log('Hello, Racoon!');
+
+let skaicius = 5; // number
+let skaicius3 = '10'; // string
+const tekstas = 'Labas, Meškėnas!'; // string
+
+
+{
+    console.log(skaicius);
+    {
+        let skaicius = 10;
+        console.log(skaicius);
+    }
+    console.log(skaicius);
+}
+
+// console.log(skaicius);
+
+console.log(typeof skaicius, typeof skaicius3, typeof tekstas);
+
+const jonas = document.querySelector('.bu');
+
+console.log(jonas, typeof jonas);
+
+
+    jonas.innerText = 'Labas, Meškėnas!';
+
+    jonas.style.color = 'crimson';
+
+
+
+
+
