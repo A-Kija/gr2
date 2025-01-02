a86ad78a43d73e60e47c2b31b187de70a135eb2f
A-Kija <arvydas.kijakauskas@gmail.com>
2025-01-02 16:54:04 +0200

    026

--- travel/server.js
+++ travel/server.js
@@ -3,11 +3,14 @@ const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const handlebars = require('handlebars');
+const cookieParser = require('cookie-parser');
+const { v4: uuidv4 } = require('uuid');
+const md5 = require('md5');
 const URL = 'http://localhost:3000/';
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Helpers
 const makeHtml = (data, page, back = true) => {
     const dir = back ? 'back' : 'front';
     data.url = URL;
@@ -18,7 +21,73 @@ const makeHtml = (data, page, back = true) => {
     return html;
 }
 
+const updateSession = (req, prop, data) => {
+    const sessionId = req.user.sessionId;
+    let sessions = fs.readFileSync('./data/session.json', 'utf8');
+    sessions = JSON.parse(sessions);
+    let session = sessions.find(s => s.sessionId === sessionId);
+    if (!session){
+        return;
+    }
+    if (null === data) {
+        delete session[prop];
+    } else {
+        session[prop] = data;
+    }
+    sessions = JSON.stringify(sessions);
+    fs.writeFileSync('./data/session.json', sessions);
+}
 
+// Midleware
+
+// Cookie middleware
+const cookieMiddleware = (req, res, next) => {
+    let visitsCount = req.cookies.visits || 0;
+    visitsCount++;
+    // ONE YEAR
+    res.cookie('visits', visitsCount, { maxAge: 1000 * 60 * 60 * 24 * 365 });
+    next();
+}
+
+// Session middleware
+const sessionMiddleware = (req, res, next) => {
+    let sessionId = req.cookies.sessionId || null;
+    if (!sessionId) {
+        sessionId = md5(uuidv4()); // md5 kad būtų trumpesnis
+    }
+    let session = fs.readFileSync('./data/session.json', 'utf8');
+    session = JSON.parse(session);
+    let user = session.find(u => u.sessionId === sessionId);
+    if (!user) {
+        user = {
+            sessionId
+        };
+        session.push(user);
+        session = JSON.stringify(session);
+        fs.writeFileSync('./data/session.json', session);
+    }
+    req.user = user;
+    res.cookie('sessionId', sessionId, { maxAge: 1000 * 60 * 60 * 24 * 365 });
+    next();
+};
+
+//Messages middleware
+const messagesMiddleware = (req, res, next) => {
+    if (req.method === 'GET') {
+        updateSession(req, 'message', null);
+    }
+    next();
+};
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cookieMiddleware);
+app.use(sessionMiddleware);
+app.use(messagesMiddleware);
+
+// Routes
 app.get('/admin', (req, res) => {
 
     const data = {
@@ -39,6 +108,7 @@ app.get('/admin/page-top', (req, res) => {
     const data = {
         pageTitle: 'Pagrindinio puslapio viršus',
         mainTopData,
+        message: req.user.message || null
     };
 
     const html = makeHtml(data, 'pageTop');
@@ -60,6 +130,8 @@ app.post('/admin/page-top', (req, res) => {
     mainTopData = JSON.stringify(mainTopData);
 
     fs.writeFileSync('./data/main-top.json', mainTopData);
+
+    updateSession(req, 'message', {text: 'Puslapis atnaujintas', type: 'success'});
 
     res.redirect(URL + 'admin/page-top');
 
@@ -84,6 +156,8 @@ app.get('/', (req, res) => {
     res.send(html);
 });
 
+
+// Start server
 
 const port = 3000;
 app.listen(port, () => {
