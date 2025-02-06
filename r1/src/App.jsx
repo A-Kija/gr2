d5024309b2ad95fe0deb562ed7d95829d762d15d
A-Kija <arvydas.kijakauskas@gmail.com>
2025-02-06 15:15:14 +0200

    047A

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,78 +1,42 @@
-import './app.scss';
 import './buttons.scss';
-import rand from './Functions/rand';
-import Sq from './Components/046/Sq';
-import { useEffect, useState, useCallback, useRef } from 'react';
-import randColor from './Functions/randColor';
+import './crud.scss';
+import Create from './Components/crud/Create';
+import { useEffect } from 'react';
+import axios from 'axios';
+import * as C from './Components/crud/constants';
+
 
 export default function App() {
 
-    const [sq, setSq] = useState([]);
-    const [sqCount, setSqCount] = useState(0);
-    const [color, setColor] = useState('yellow');
-    const loaded = useRef(false);
+    const [storeData, setStoreData] = useState(null);
 
-    const add = _ => {
-        setSq(s => [...s, rand(1000, 9999)]);
-        // setSqCount(sq.length + 1); nelabai
-    }
 
-    const add2 = _ => {
-        setSq(s => [...s, rand(1000, 9999), rand(1000, 9999)]);
-        // setSqCount(sq.length + 2); nelabai
-    }
 
-    const myFun = useCallback(_ => {
-        console.log('My fun');
-    }, []);
 
     useEffect(_ => {
-        console.log('App komponentas atnaujintas');
-        // setSqCount(sq.length); nelabai
-    });
-    // šis efektas bus paleistas kiekvieną kartą, kai komponentas atnaujinamas
-    // nelabai naudingas 
-
-    useEffect(_ => {
-        console.log('App komponentas užkrautas');
-    }, []);
-    // tuščias masyvas reiškia, kad šis efektas bus paleistas tik vieną kartą, kai komponentas užkraunamas
-
-    useEffect(_ => {
-        if (!loaded.current) {
-            loaded.current = true;
+        if (null === storeData) {
             return;
-        } // išvengiama pirmojo paleidimo, kai komponentas užkraunamas
+        }
+        axios.post(C.serverUrl, storeData)
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
 
-        console.log('Kvadratukai atsinaujino');
-        myFun();
-        setSqCount(sq.length);
-
-        // setSq(s => [...s, rand(1000, 9999)]);
-        // Užsiciklinimas, nes šis efektas keičia kvadratukų masyvą, o kvadratukų masyvas kviečia efektą
-
-    }, [sq, myFun]);
-    // šis efektas bus paleistas kiekvieną kartą, kai atnaujinamas kvadratukų masyvas
-
+    }, [storeData]);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1 style={{ color: color }}>Hello, Vinted!</h1>
-
-                <h1>Count: {sqCount}</h1>
-                <div className="sq-bin">
-                    {
-                        sq.map((n, i) => <Sq key={i} num={n} />)
-                    }
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <Create />
                 </div>
-                <button className="yellow" onClick={add}>Add</button>
-                <button className="green" onClick={add2}>Add 2</button>
-                <button className="red" onClick={_ => setColor(randColor())}>Change color</button>
-
-            </header>
+                <div className="col-8">
+                    List
+                </div>
+            </div>
         </div>
     );
 }
-
-// 1. Keičiantis kvadratukų skaičiui reikia atnaujinti ir kvadratukų skaičių sqCount
---
+++ r1/src/Components/crud/constants.js
@@ -0,0 +1,7 @@
+export const defaultPlanet = {
+    name: '',
+    size: 100,
+    color: '000000'
+};
+
+export const serverUrl = 'http://localhost:3333/';
