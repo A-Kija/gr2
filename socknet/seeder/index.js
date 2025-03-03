6448fa8496630ef073ec51d2598b7a1983d929e8
A-Kija <arvydas.kijakauskas@gmail.com>
2025-03-03 16:42:48 +0200

    060

--- socknet/seeder/index.js
+++ socknet/seeder/index.js
@@ -2,7 +2,9 @@ console.log('start seeding');
 import { faker } from '@faker-js/faker';
 import { createUser } from './user.js';
 import { createPost } from './post.js';
-import { makeLikes } from './functions.js';
+import { makeLikes, makeMessagesUsers } from './functions.js';
+import { createImage } from './image.js';
+import { createMessage } from './message.js';
 import mysql from 'mysql';
 
 
@@ -18,9 +20,57 @@ const posts = faker.helpers.multiple(createPost, {
     count: postsCount,
 });
 
-posts.forEach(p => {
+const images = [];
+const messages = [];
+
+users.forEach((_, key) => {
+    const toUserId = key + 1;
+    const fromUsers = makeMessagesUsers(toUserId, usersCount);
+
+    fromUsers.forEach(fromUserId => {
+        if (!messages.some(msg => {
+            (msg.toUserId === toUserId && msg.fromUserId === fromUserId)
+                ||
+                (msg.toUserId === fromUserId && msg.fromUserId === toUserId)
+        })
+        ) {
+            const endTime = faker.date.recent({ days: 5 });
+            const replies = faker.number.int({ min: 1, max: 50 });
+            let seenTo = true;
+            let seenFrom = !faker.number.int({ min: 0, max: 1 });
+            const seen = [];
+            seenTo && seen.push(toUserId);
+            seenFrom && seen.push(fromUserId);
+            // d1.setMinutes(d1.getMinutes() - 879);
+            messages.push({
+                ...createMessage(),
+                toUserId,
+                fromUserId,
+                seen,
+                created_at: endTime
+            });
+            for (let i = 0; i < replies; i++) {
+                //
+            }
+        }
+    });
+});
+
+
+
+posts.forEach((p, key) => {
     p.user_id = faker.number.int({ min: 1, max: usersCount });
     p.votes = JSON.stringify(makeLikes(usersCount));
+
+    // add images
+    const imagesCount = faker.number.int({ min: 1, max: 5 });
+    for (let i = 0; i < imagesCount; i++) {
+        images.push({
+            ...createImage(),
+            post_id: key + 1,
+            main: !i ? 1 : 0
+        });
+    }
 });
 
 
@@ -39,6 +89,15 @@ con.connect(function (err) {
 
 let sql;
 
+sql = 'DROP TABLE IF EXISTS images;'
+con.query(sql, (err) => {
+    if (err) {
+        console.log('Images table drop error', err);
+    } else {
+        console.log('Images table was dropped');
+    }
+});
+
 sql = 'DROP TABLE IF EXISTS posts;'
 con.query(sql, (err) => {
     if (err) {
@@ -47,7 +106,6 @@ con.query(sql, (err) => {
         console.log('Posts table was dropped');
     }
 });
-
 
 sql = 'DROP TABLE IF EXISTS users;'
 con.query(sql, (err) => {
@@ -73,9 +131,9 @@ sql = `
 `;
 con.query(sql, (err) => {
     if (err) {
-        console.log('Users table create error', err)
+        console.log('Users table create error', err);
     } else {
-        console.log('Users table was created')
+        console.log('Users table was created');
     }
 });
 
@@ -91,12 +149,28 @@ CREATE TABLE posts (
 `;
 con.query(sql, (err) => {
     if (err) {
-        console.log('Posts table create error', err)
+        console.log('Posts table create error', err);
     } else {
-        console.log('Posts table was created')
+        console.log('Posts table was created');
     }
 });
 
+sql = `
+CREATE TABLE images (
+  id int(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  url varchar(100) NOT NULL,
+  post_id int(10) UNSIGNED NOT NULL,
+  main tinyint(1) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+`;
+
+con.query(sql, (err) => {
+    if (err) {
+        console.log('Images table create error', err);
+    } else {
+        console.log('Images table was created');
+    }
+});
 
 
 sql = `
@@ -106,9 +180,9 @@ sql = `
 `;
 con.query(sql, [users.map(user => [user.name, user.email, user.password, user.role, user.avatar, user.created_at, user.status, user.online])], (err) => {
     if (err) {
-        console.log('Users table seed error', err)
+        console.log('Users table seed error', err);
     } else {
-        console.log('Users table was seeded')
+        console.log('Users table was seeded');
     }
 });
 
@@ -119,9 +193,24 @@ sql = `
 `;
 con.query(sql, [posts.map(post => [post.content, post.created_at, post.updated_at, post.votes, post.user_id])], (err) => {
     if (err) {
-        console.log('Posts table seed error', err)
+        console.log('Posts table seed error', err);
     } else {
-        console.log('Posts table was seeded')
+        console.log('Posts table was seeded');
+    }
+});
+
+
+sql = `
+    -- Ką tu
+    INSERT INTO images
+    (url, post_id, main)
+    VALUES ?
+`;
+con.query(sql, [images.map(image => [image.url, image.post_id, image.main])], (err) => {
+    if (err) {
+        console.log('Images table seed error', err);
+    } else {
+        console.log('Images table was seeded');
     }
 });
 
@@ -130,9 +219,3 @@ con.query(sql, [posts.map(post => [post.content, post.created_at, post.updated_a
 
 
 con.end();
-
-
-
-
-// console.log(users);
-
